992b8a6cdd9c37963ba206610a6fa8d207df0c32
Juliette <97114875+Juliette02@users.noreply.github.com>
2022-05-03 14:30:43 +0200

    SQL

---
+++ BDD/insert_Ju.sql
@@ -0,0 +1,27 @@
+insert into Produit (lib_cou_pro, lib_long_pro, ref_fou_pro, pho_pro, pri_ach_pro, pri_ht_pro, ctg_pro, id_rub, id_liv, id_fou) Values
+
+('ETC S4 PAR EA','ETC S4 PAR EA','Musique','ETC.jpg',150,256.8,'Projecteur',27,NULL,1),
+('Eurolite LED SL-400','Eurolite LED SL-400 DMX Search Light','Musique','Eurolite.jpg',1200,1500,'Projecteur',27,NULL,1),
+('Cameo Matrix 10W','Cameo Matrix Panel 10 W RGB','Musique','Cameo.jpg',400,431.2,'Projecteur',27,NULL,1,1),
+('Stairville 132 LED RGB','Stairville Wild Wash 132 LED RGB','Musique','Stairville.jpg',50,63.2,'Stroboscopes',28,NULL,1),
+('Martin Atomic','Martin Atomic 3000 LED','Musique','Martin.jpg',1900,2318.4,'Stroboscopes',28,NULL,1),
+('Varytec Colors','Varytec Colors SonicStrobe','Musique','Varytec.jpg',100,141.6, 'Stroboscopes',28,NULL,1),
+('Laserworld DS-1000RGB','Laserworld DS-1000RGB MK3','Musique','Laserworld.jpg',500,596,'Lasers',29,NULL,1),
+('ADj Stinger','ADJ Stinger II','Musique','ADj.jpg',150,172,'Lasers',29,NULL,1),
+('Laserworld PL','Laserworld PL-5000RGB MK2','Vivre Art Loux','Laserworld1.jpg',1900,2199.2,'Lasers',29,NULL,4),
+('Gravity LTS', 'Gravity LTS 01 B Set','Vivre Art Loux','Gravity.jpg',40,61.6,'Supports',30,NULL,4),
+('Roadworx Electric','Roadworx Multi Electric Stand','Vivre Art Loux','Roadworx.jpg',200,231.2,'Supports',30,NULL,30,4),
+('Standback','Standback Ampstand','Musique','Standback.jpg',15,23.2,'Supports',30,NULL,1),
+('Millenium MDT5S-Pro','Millenium MDT5S-Pro Drum Throne Saddle','Musique Percussion','Millenium.jpg',50,55.2,'Sièges',NULL,31,3),
+('Mapex MXT765ASER ','Mapex MXT765ASER Drum Stool','Musique Percussion','Mapex.jpg',100,127.2,'Sièges',NULL,31,3),
+('DW 9120AL','DW 9120AL Drum Stool','Musique Percussion','DW.jpg',200,255.2,'Sièges',NULL,31,3),
+('Boss Guitar Headphones','Boss Waza Air Guitar Headphones','Vivre Art Loux','Boss.jpg',250,303.2,'Casque audio',NULL,32,3),
+('Metrophones MPD-G','Metrophones MPD-G Headphones LCD Metronom','Musique Percussion','Metrophones.jpg',120,168.8,'Casque audio',NULL,32,3),
+('Superlux HD-681','Superlux HD-681','Le musique en ville','Superlux.jpg',10,18.08,'Casque audio',NULL,32,5);
+
+('Pearl EXX725SBR/C','Pearl EXX725SBR/C Export Aztec Gold','Musique Percussion','Pearl.jpg',450,599.2,'Batteries Acoustiques',NULL,12,3),
+('Tama Star Bubinga','Tama STAR Drum Bubinga White Ebony','Musique Percussion','Tama.jpg',6000,7199-2,'Batteries Acoustiques',NULL,12,3),
+('Sonor AQX Jungle','Sonor AQX Jungle Set BOS','La musique en ville','Sonor.jpg',350,299.2,'Batteries Acoustiques',NULL,12,5),
+('Roland VAD706-GC Bundle','Roland VAD706-GC E-Drum Set Bundle','Comme aux Musiques','Roland.jpg',6000,6872,'Batteries Eletroniques',NULL,13,2),
+('Alesis Strike Pro','Alesis Strike Pro Special Edition','Musique Percussion','Alesis.jpg',1500,1887.2,'Batteries Electroniques',NULL,13,3),
+('Millenium E-Drum Set','Millenium HD-120 E-Drum Set','Musique Percussion','Millenium1.jpg',100,158.4,'Batteries Electroniques', NULL,13,3);
