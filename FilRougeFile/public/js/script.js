18127213d5228f77e9f43f71fc4578dd5afa11cf
Juliette <97114875+Juliette02@users.noreply.github.com>
2022-08-22 12:54:48 +0200

    22/08/2022 JS register

--- FilRougeFile/public/js/script.js
+++
@@ -1,8 +0,0 @@
-$("#new_edit_utilisateur").on('submit', function(){
-    if($("#utilisateur_password").val() != $("#verifpass").val()) {
-        //implémntez votre code
-        alert("Les deux mots de passe saisies sont différents");
-        alert("Merci de renouveler l'opération");
-        return false;
-    }
-})
